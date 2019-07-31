5726dcb29bbe0d11d2ac5f25fb2856d6c738db47
Benjamin Chan <bchan1298@gmail.com>
2019-07-31 15:17:21 -0400

    Added code to sort the plasmid part order with SequenceConstraints

--- src/componentbuckets.py
+++ src/componentbuckets.py
@@ -170,9 +170,17 @@ def addPlasmidParts(doc, originalCDs, device_test, device, plasmidPartDictionary
                 dList.append(fc)
                 dName += fc.displayId.replace(name + '_', '') + '__'
 
+        def comparison(self, other):
+            return self.precedes(other)
+
+        SequenceAnnotation.__lt__ = comparison # noqa
+
         if len(dtList) != 0:
             dtCD = ComponentDefinition(dtName[:-2]) # noqa
             fcList = []
+
+            CSADictionary = {}
+            saList = []
 
             for fc in dtList:
                 c = dtCD.components.create(fc.displayId)
@@ -182,11 +190,24 @@ def addPlasmidParts(doc, originalCDs, device_test, device, plasmidPartDictionary
                 fcList.append(fc.displayId)
 
                 for sa in plasmid.sequenceAnnotations:
-                    if sa.name == fc.displayId:
-                        newSA = dtCD.sequenceAnnotations.create(sa.displayId)  #add(sa.copy())
-                        newSA.name = sa.name
-                        for location in sa.locations:
-                            newSA.locations.add(location)
+                    if sa.name.replace(' ', '_') == fc.displayId:
+                        saList.append(sa)
+                        CSADictionary[sa.name] = c
+
+            saList.sort()
+
+            for index, sa in enumerate(saList):
+                if index == len(saList) - 1:
+                    break
+
+                nextComponent = saList[index + 1]
+
+                scName = str(sa.name.replace(' ', '_') + '__' +
+                             nextComponent.name.replace(' ', '_'))
+                sc = dtCD.sequenceConstraints.create(scName)
+                sc.subject = CSADictionary[sa.name].identity
+                sc.object = CSADictionary[nextComponent.name].identity
+                sc.restriction = SBOL_RESTRICTION_PRECEDES # noqa
 
             try:
                 doc.addComponentDefinition(dtCD)
@@ -203,6 +224,9 @@ def addPlasmidParts(doc, originalCDs, device_test, device, plasmidPartDictionary
             dCD = ComponentDefinition(dName[:-2]) # noqa
             fcList = []
 
+            CSADictionary = {}
+            saList = []
+
             for fc in dList:
                 c = dCD.components.create(fc.displayId)
                 c.definition = fc.definition
@@ -211,18 +235,24 @@ def addPlasmidParts(doc, originalCDs, device_test, device, plasmidPartDictionary
                 fcList.append(fc.displayId)
 
                 for sa in plasmid.sequenceAnnotations:
-                    if sa.name == fc.displayId:
-                        # dCD.sequenceAnnotation.add(sa)
-                        newSA = dCD.sequenceAnnotations.create(sa.displayId)  #add(sa.copy())
-                        newSA.name = sa.name
-                        for location in sa.locations:
-                            newSA.locations.create('testing')
-                        # newSA = dCD.sequenceAnnotations.create(sa.displayId)  #add(sa.copy())
-                        # newSA.name = sa.name
-                        # for location in sa.locations:
-                        #     newRange = Range(location.disola)
-                        #     newLocation = newSA.locations.create(location.displayId)
-                        #     newLocation.name
+                    if sa.name.replace(' ', '_') == fc.displayId:
+                        saList.append(sa)
+                        CSADictionary[sa.name] = c
+
+            saList.sort()
+
+            for index, sa in enumerate(saList):
+                if index == len(saList) - 1:
+                    break
+
+                nextComponent = saList[index + 1]
+
+                scName = str(sa.name.replace(' ', '_') + '__' +
+                             nextComponent.name.replace(' ', '_'))
+                sc = dCD.sequenceConstraints.create(scName)
+                sc.subject = CSADictionary[sa.name].identity
+                sc.object = CSADictionary[nextComponent.name].identity
+                sc.restriction = SBOL_RESTRICTION_PRECEDES # noqa
 
             try:
                 doc.addComponentDefinition(dCD)
